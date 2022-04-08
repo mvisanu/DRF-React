2cd54e75f4bf35a3c22e0a797591b306fc1d4aed
Visanu <mvisanu@gmail.com>
2022-04-08 08:54:37 -0500

    Add login, logout, register and axios

--- src/index.js
+++ src/index.js
@@ -4,8 +4,11 @@ import * as serviceWorker from './serviceWorker';
 import './index.css';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import App from './App';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './components/header';
+import Footer from './components/footer';
+import Register from './components/register';
+import Login from './components/login';
+import Logout from './components/logout';
 //import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -16,6 +19,9 @@ const routing = (
 			<Header />
 			<Routes>
 				<Route exact path="/" element={<App />} />
+				<Route path="/register" component={Register} />
+				<Route path="/login" component={Login} />
+				<Route path="/logout" component={Logout} />
 			</Routes>
 			<Footer />
 		</React.StrictMode>
